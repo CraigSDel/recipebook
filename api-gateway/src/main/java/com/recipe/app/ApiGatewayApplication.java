ede14f58ce95b169ab03f336151b64da67e779bd
Craig Stroberg <strobergcd@gmail.com>
2021-01-20 15:23:02 +0200

    Parent pom builds

--- api-gateway/src/main/java/com/recipe/app/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/recipe/app/ApiGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
